9d5aa2be01404298d88905fda815bd1568c78a0f
danielburgosr96 <danielburgosr96@gmail.com>
2021-03-12 18:59:01 -0500

    Add - AddJugador and CrearJuego use case

--- no_me_mientas/src/main/java/org/example/usecase/AddJugadorUseCase.java
+++ no_me_mientas/src/main/java/org/example/usecase/AddJugadorUseCase.java
@@ -1,27 +1,32 @@
 package org.example.usecase;
 
+import co.com.sofka.business.generic.BusinessException;
 import co.com.sofka.business.generic.UseCase;
 import co.com.sofka.business.support.RequestCommand;
 import co.com.sofka.business.support.ResponseEvents;
 import org.example.domain.juego.Juego;
-import org.example.domain.juego.command.AñadirJugador;
+import org.example.domain.juego.command.AddJugador;
 
-public class AñadirJugadorUseCase extends UseCase<RequestCommand<AñadirJugador>, ResponseEvents> {
+public class AddJugadorUseCase extends UseCase<RequestCommand<AddJugador>, ResponseEvents> {
 
 
     @Override
-    public void executeUseCase(RequestCommand<AñadirJugador> añadirJugadorRequestCommand) {
+    public void executeUseCase(RequestCommand<AddJugador> añadirJugadorRequestCommand) {
 
         var command = añadirJugadorRequestCommand.getCommand();
+
         var juegoId = command.getJuegoId();
-        var jugador = command.getJugador();
+        var jugadorId = command.getJugadorId();
+        var nombre = command.getNombre();
 
         var juego = Juego.from(juegoId, retrieveEvents());
-        juego.añadirJugador(jugador);
 
-        var commits = juego.getUncommittedChanges();
+        if(juego.jugadores().size() > 23){
+            throw new BusinessException(juegoId.value(), "No puede haber más de 24 jugadores");
+        }
 
-        emit().onResponse(new ResponseEvents(commits));
+        juego.addJugador(jugadorId, nombre);
 
+        emit().onResponse(new ResponseEvents(juego.getUncommittedChanges()));
     }
 }
--- no_me_mientas/src/main/java/org/example/usecase/CrearJuegoUseCase.java
+++ no_me_mientas/src/main/java/org/example/usecase/CrearJuegoUseCase.java
@@ -1,2 +1,23 @@
-package org.example.usecase;public class CrearJuegoUseCase {
+package org.example.usecase;
+
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+import org.example.domain.juego.Juego;
+import org.example.domain.juego.command.CrearJuego;
+import org.example.domain.juego.values.JuegoId;
+
+public class CrearJuegoUseCase extends UseCase<RequestCommand<CrearJuego>, ResponseEvents> {
+
+    @Override
+    public void executeUseCase(RequestCommand<CrearJuego> crearJuegoRequestCommand) {
+
+        var command = crearJuegoRequestCommand.getCommand();
+        var juegoId = new JuegoId();
+        var juego = new Juego(juegoId, command.getJugadores());
+
+        var commits = juego.getUncommittedChanges();
+
+        emit().onResponse(new ResponseEvents(commits));
+    }
 }
