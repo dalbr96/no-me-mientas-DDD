97f106997df1512f9166dcad054fd336b9a15586
danielburgosr96 <danielburgosr96@gmail.com>
2021-03-12 09:42:45 -0500

    Add Etapa.java entity

--- no_me_mientas/src/main/java/org/example/domain/ronda/Etapa.java
+++ no_me_mientas/src/main/java/org/example/domain/ronda/Etapa.java
@@ -1,2 +1,12 @@
-package org.example.domain.ronda.values;public class Etapa {
+package org.example.domain.ronda;
+
+import co.com.sofka.domain.generic.Entity;
+import org.example.domain.ronda.values.EtapaId;
+
+public class Etapa extends Entity<EtapaId> {
+
+    private Etapa(EtapaId entityId) {
+        super(entityId);
+    }
+
 }
