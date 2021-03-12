ce26f32011c6860183de47b1b5ca058090c5a27e
danielburgosr96 <danielburgosr96@gmail.com>
2021-03-12 14:04:05 -0500

    Add - VO to Etapa entity

--- no_me_mientas/src/main/java/org/example/domain/ronda/Etapa.java
+++ no_me_mientas/src/main/java/org/example/domain/ronda/Etapa.java
@@ -1,12 +1,24 @@
 package org.example.domain.ronda;
 
 import co.com.sofka.domain.generic.Entity;
+import org.example.domain.juego.Jugador;
+import org.example.domain.juego.values.Dinero;
+import org.example.domain.juego.values.JugadorId;
+import org.example.domain.ronda.values.Apuesta;
 import org.example.domain.ronda.values.EtapaId;
+
+import java.util.Map;
 
 public class Etapa extends Entity<EtapaId> {
 
-    private Etapa(EtapaId entityId) {
+    private final Map<JugadorId, Jugador> participantesEtapa;
+    private Dinero apuestaMaxima;
+    private Map<JugadorId, Apuesta> turnos;
+
+
+    private Etapa(EtapaId entityId, Map<JugadorId, Jugador> participantesEtapa) {
         super(entityId);
+        this.participantesEtapa = participantesEtapa;
     }
 
 }
