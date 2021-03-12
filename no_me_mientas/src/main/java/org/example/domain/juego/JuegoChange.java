6038ed54605625e07f7fa39a9f9ce317104a0e65
danielburgosr96 <danielburgosr96@gmail.com>
2021-03-12 08:17:45 -0500

    Add - apply for JuegoCreado and JugadorAnhadido in JuegoChange.java

--- no_me_mientas/src/main/java/org/example/domain/juego/JuegoChange.java
+++ no_me_mientas/src/main/java/org/example/domain/juego/JuegoChange.java
@@ -1,10 +1,22 @@
 package org.example.domain.juego;
 
 import co.com.sofka.domain.generic.EventChange;
+import org.example.domain.juego.events.JuegoCreado;
+import org.example.domain.juego.events.JugadorAnhadido;
 
 public class JuegoChange extends EventChange {
+
     public JuegoChange(Juego juego){
 
+        apply((JuegoCreado event) ->{
+            juego.jugadores = event.getJugadores();
+            juego.juegoIniciado = Boolean.FALSE;
+            juego.tieneGanador = Boolean.FALSE;
+        });
+
+        apply((JugadorAnhadido event) -> {
+            juego.jugadores.put(event.getJugador().identity(), event.getJugador());
+        });
     }
 
 }
