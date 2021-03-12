07120f3c07dd7f4ef8ad1e067da5bf595affb80b
danielburgosr96 <danielburgosr96@gmail.com>
2021-03-12 08:29:54 -0500

    Add - application of JuegoIniciado event on JuegoChange.java

--- no_me_mientas/src/main/java/org/example/domain/juego/JuegoChange.java
+++ no_me_mientas/src/main/java/org/example/domain/juego/JuegoChange.java
@@ -2,6 +2,7 @@ package org.example.domain.juego;
 
 import co.com.sofka.domain.generic.EventChange;
 import org.example.domain.juego.events.JuegoCreado;
+import org.example.domain.juego.events.JuegoIniciado;
 import org.example.domain.juego.events.JugadorAnhadido;
 
 public class JuegoChange extends EventChange {
@@ -16,6 +17,16 @@ public class JuegoChange extends EventChange {
 
         apply((JugadorAnhadido event) -> {
             juego.jugadores.put(event.getJugador().identity(), event.getJugador());
+        });
+
+        apply((JuegoIniciado event) ->{
+
+            if (Boolean.TRUE.equals(juego.juegoIniciado)){
+
+                throw new IllegalArgumentException("El juego ya está iniciado");
+            }
+
+            juego.juegoIniciado = Boolean.TRUE;
         });
     }
 
