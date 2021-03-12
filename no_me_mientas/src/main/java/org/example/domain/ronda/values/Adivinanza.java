1c913ba31f7a459119855ce0a097bf0a07ee6af1
danielburgosr96 <danielburgosr96@gmail.com>
2021-03-12 14:04:47 -0500

    Add - Apuesta and Adivinanza object values

--- no_me_mientas/src/main/java/org/example/domain/ronda/values/Adivinanza.java
+++ no_me_mientas/src/main/java/org/example/domain/ronda/values/Adivinanza.java
@@ -1,2 +1,33 @@
-package org.example.domain.ronda.values;public class Adivinanza {
+package org.example.domain.ronda.values;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class Adivinanza implements ValueObject<Adivinanza.Values> {
+
+    private final Integer cara;
+    private final Integer numeroRepeticiones;
+
+    public Adivinanza(Integer cara, Integer numeroRepeticiones){
+        this.cara = Objects.requireNonNull(cara, "Debe proveer una cara");
+        if(cara > 6 || cara < 1){
+            throw new IllegalArgumentException("La cara debe ser un numero entre 1 y 6");
+        }
+        this.numeroRepeticiones = Objects.requireNonNull(numeroRepeticiones, "Debe proveer el numero de repeticiones");
+        if(numeroRepeticiones < 3){
+            throw new IllegalArgumentException("El numero de repeticiones debe ser de al menos 3");
+        }
+
+    }
+
+    @Override
+    public Values value() {
+        return null;
+    }
+
+    public interface Values {
+        Integer cara();
+        Integer carasRepetidas();
+    }
 }
--- no_me_mientas/src/main/java/org/example/domain/ronda/values/Apuesta.java
+++ no_me_mientas/src/main/java/org/example/domain/ronda/values/Apuesta.java
@@ -1,2 +1,44 @@
-package org.example.domain.ronda.values;public class Apuesta {
+package org.example.domain.ronda.values;
+
+import co.com.sofka.domain.generic.ValueObject;
+import org.example.domain.juego.values.Dinero;
+
+import java.util.Objects;
+
+
+public class Apuesta implements ValueObject<Apuesta.Values> {
+
+    private final Dinero dineroApostado;
+    private final Adivinanza adivinanza;
+
+    public Apuesta(Dinero dineroApostado, Adivinanza adivinanza){
+
+        this.dineroApostado = Objects.requireNonNull(dineroApostado);
+        this.adivinanza = Objects.requireNonNull(adivinanza, "Debe proveer una adivinanza");
+
+    }
+
+
+    @Override
+    public Values value() {
+        return new Values(){
+
+            @Override
+            public Dinero dineroApostado() {
+                return dineroApostado;
+            }
+
+            @Override
+            public Adivinanza adivinanzaRealizada() {
+                return adivinanza;
+            }
+        };
+    }
+
+    public interface Values {
+
+        Dinero dineroApostado();
+        Adivinanza adivinanzaRealizada();
+
+    }
 }
