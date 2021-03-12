560ae1aa3fea619832febbc0c99b50f13ad6de5c
danielburgosr96 <danielburgosr96@gmail.com>
2021-03-12 08:05:43 -0500

    Add - JuegoCreado event

--- no_me_mientas/src/main/java/org/example/domain/juego/events/JuegoCreado.java
+++ no_me_mientas/src/main/java/org/example/domain/juego/events/JuegoCreado.java
@@ -1,2 +1,16 @@
-package org.example.domain.juego.events;public class JuegoCreado {
+package org.example.domain.juego.events;
+
+import co.com.sofka.domain.generic.DomainEvent;
+import org.example.domain.juego.Jugador;
+import org.example.domain.juego.values.JugadorId;
+
+import java.util.Map;
+
+public class JuegoCreado extends DomainEvent {
+
+    private final Map<JugadorId, Jugador> jugadores;
+    public JuegoCreado(Map<JugadorId, Jugador> jugadores) {
+        super("nomemientas.JuegoCreado");
+        this.jugadores = jugadores;
+    }
 }
