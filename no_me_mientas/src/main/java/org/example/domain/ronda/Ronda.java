edf077c08fe47ddbc73b8e02776b63471f6c5f34
danielburgosr96 <danielburgosr96@gmail.com>
2021-03-12 09:41:07 -0500

    Add - Ronda.java

--- no_me_mientas/src/main/java/org/example/domain/ronda/Ronda.java
+++ no_me_mientas/src/main/java/org/example/domain/ronda/Ronda.java
@@ -1,2 +1,28 @@
-package org.example.domain.ronda;public class Ronda {
+package org.example.domain.ronda;
+
+import co.com.sofka.domain.generic.AggregateEvent;
+import org.example.domain.juego.Jugador;
+import org.example.domain.juego.values.Dinero;
+import org.example.domain.juego.values.JugadorId;
+import org.example.domain.ronda.values.Dado;
+import org.example.domain.ronda.values.Puntaje;
+import org.example.domain.ronda.values.RondaId;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Ronda extends AggregateEvent<RondaId> {
+
+    protected Map<JugadorId, Jugador> jugadoresRonda;
+    protected Map<JugadorId, Puntaje> puntajes;
+    protected Dinero capitalAcumulado;
+    protected Set<Etapa> etapas;
+    protected List<Dado> dados;
+
+
+    public Ronda(RondaId entityId) {
+        super(entityId);
+    }
+
 }
