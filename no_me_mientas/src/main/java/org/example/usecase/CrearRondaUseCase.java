33136db63e62974c37dca7b7c2ca05f7ac8a6df5
danielburgosr96 <danielburgosr96@gmail.com>
2021-03-13 11:49:27 -0500

    Add - CrearRondaUseCase

--- no_me_mientas/src/main/java/org/example/usecase/CrearRondaUseCase.java
+++ no_me_mientas/src/main/java/org/example/usecase/CrearRondaUseCase.java
@@ -1,2 +1,25 @@
-package org.example.usecase;public class CrearRondaUseCase {
+package org.example.usecase;
+
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+import org.example.domain.ronda.Ronda;
+import org.example.domain.ronda.command.CrearRonda;
+import org.example.domain.ronda.values.RondaId;
+
+public class CrearRondaUseCase extends UseCase<RequestCommand<CrearRonda>, ResponseEvents> {
+
+    @Override
+    public void executeUseCase(RequestCommand<CrearRonda> crearRondaRequestCommand) {
+
+        var command = crearRondaRequestCommand.getCommand();
+        var rondaId = new RondaId();
+
+        var ronda = new Ronda(rondaId, command.getJugadores());
+
+        var commits = ronda.getUncommittedChanges();
+
+        emit().onResponse(new ResponseEvents(commits));
+
+    }
 }
