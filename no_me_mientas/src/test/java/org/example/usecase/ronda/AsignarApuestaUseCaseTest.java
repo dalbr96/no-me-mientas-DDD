6ddb78ce8210a13d1e67e6446e5327699269699f
danielburgosr96 <danielburgosr96@gmail.com>
2021-03-19 17:21:28 -0500

    Change AsignarApuesta test

--- no_me_mientas/src/test/java/org/example/usecase/ronda/AsignarApuestaUseCaseTest.java
+++ no_me_mientas/src/test/java/org/example/usecase/ronda/AsignarApuestaUseCaseTest.java
@@ -24,12 +24,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 
